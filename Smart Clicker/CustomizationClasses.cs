85be83da9610bf07e2d7ba76d6a37b6747f6dcd5
Andres Romero <andresr@mit.edu>
2013-10-29 14:06:42 -0400

    Added customization classes
    Will become shared instances between different forms and threads - will
    have to deal with loading and saving from file as well as concurrency at
    some later point.

---
+++ Smart Clicker/CustomizationClasses.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Smart_Clicker
+{
+    class CustomizationParameters
+    {
+        public ClickCustomization clickValues;
+        public LayoutCustomization layoutValues;
+        public ContextCustomization contextValues;
+
+        public CustomizationParameters()
+        {
+            // To be replaced by load from file!
+            this.clickValues = new ClickCustomization();
+            this.layoutValues = new LayoutCustomization();
+            this.contextValues = new ContextCustomization();
+        }
+    }
+
+    class ClickCustomization
+    {
+        // In ms
+        public int timeout;
+        // In pixels
+        public int clickBoundingBox;
+    }
+
+    class LayoutCustomization
+    {
+        public List<PictureBox> orderedIcons;
+        int startWidth;
+        int startHeight;
+    }
+
+    class ContextCustomization
+    {
+        public List<Bitmap> clickAndDragBitmaps;
+        public List<Bitmap> doubleClickBitmaps;
+        public List<Bitmap> rightClickBitmaps;
+    }
+}
